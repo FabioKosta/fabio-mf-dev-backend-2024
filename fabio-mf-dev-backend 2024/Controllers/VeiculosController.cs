edf4fc7f61fe888e16f8740644113935d32803f6
Fabio Kosta <fabiotenet@yahoo.com.br>
2024-04-20 19:38:53 -0300

    Adicionada função editar veículo

--- fabio-mf-dev-backend 2024/Controllers/VeiculosController.cs
+++ fabio-mf-dev-backend 2024/Controllers/VeiculosController.cs
@@ -31,10 +31,38 @@ namespace fabio_mf_dev_backend_2024.Controllers
                 _context.Veiculos.Add(veiculo);
                 await _context.SaveChangesAsync();
                 return RedirectToAction("Index");
-
             }
             
             return View(veiculo);
+        }
+
+        public async  Task<IActionResult> Edit(int? id) 
+        {
+            if(id == null)
+                return NotFound();
+
+            var dados = await _context.Veiculos.FindAsync(id);
+
+            if (dados == null)
+                return NotFound();
+
+            return View(dados);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id,   Veiculo veiculo)
+        {
+            if(id != veiculo.Id)
+                    return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                _context.Veiculos.Update(veiculo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View();
         }
 
     }
