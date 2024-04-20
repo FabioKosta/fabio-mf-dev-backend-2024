82f01470f80cf25cd3e713125d42b858bb75bc87
Fabio Kosta <fabiotenet@yahoo.com.br>
2024-04-20 11:48:32 -0300

    Adicionar arquivos de projeto.

---
+++ fabio-mf-dev-backend 2024/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace fabio_mf_dev_backend_2024.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
